f161bbc85a438e9dd9dddbfa23748b2a4c4e967b
Abbiechebet <abigaelchebet85@gmail.com>
2022-06-30 14:05:21 +0300

    Added List

--- HelloWorldClass.java
+++ HelloWorldClass.java
@@ -3,4 +3,7 @@ public class HelloWorldClass {
  public static void main(String[] args)
         {
             System.out.println("Hello world!");
+			System.out.println("Hello world!");
+			System.out.println("Emegency!");
+			System.out.println("list");
         }
