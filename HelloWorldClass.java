ca6be4cb808e5e905462306d1bb3660e9d63b398
Abbiechebet <abigaelchebet85@gmail.com>
2022-06-30 13:56:49 +0300

    Emegency fix

--- HelloWorldClass.java
+++ HelloWorldClass.java
@@ -3,4 +3,6 @@ public class HelloWorldClass {
  public static void main(String[] args)
         {
             System.out.println("Hello world!");
+			System.out.println("Hello world!");
+			System.out.println("Emegency!");
         }
